24e60e3379917871b59782a6cc629f187627c08e
ManuelCyrus <manuelcyrus.mda@gmail.com>
2024-11-11 09:28:25 +0100

    sd

--- sw.js
+++ sw.js
@@ -11,43 +11,19 @@ self.addEventListener('install', (e) => {
   e.waitUntil(cache);
 });
 
+self.addEventListener('fetch', function (event) {
 
-
-
-
-// Ativa o Service Worker e remove caches antigos
-self.addEventListener("activate", (event) => {
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames
-          .filter((cacheName) => cacheName !== CACHE_NAME)
-          .map((cacheName) => caches.delete(cacheName))
-      );
-    })
-  );
-});
-
-// Intercepta requisições de rede e responde com o cache, se disponível
-self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      return (
-        response ||
-        fetch(event.request)
-          .then((networkResponse) => {
-            return caches.open(CACHE_NAME).then((cache) => {
-              cache.put(event.request, networkResponse.clone());
-              return networkResponse;
-            });
-          })
-          .catch(() => {
-            return new Response(
-              "Conteúdo não disponível offline",
-              { status: 408, statusText: "Recurso indisponível" }
-            );
-          })
-      );
+
+    caches.open(cacheName).then(function (cache) {
+      return cache.match(event.request).then(function (response) {
+        return response || fetch(event.request).then(function (response) {
+          cache.put(event.request, response.clone());
+          return response;
+        });
+      });
     })
+
   );
+
 });
