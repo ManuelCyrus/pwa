303ba07fd927014145367a14cd4ea7efa5b333b4
ManuelCyrus <manuelcyrus.mda@gmail.com>
2024-11-11 13:38:00 +0100

    s
    s

--- sw.js
+++ sw.js
@@ -1,29 +1,40 @@
-let cacheName = 'cache-v1';
+const CACHE_NAME = 'app-cache-v1';
+const urlsToCache = [
+  '',
+  'index.html',
+  'estilo.css',
+  'icone.png'
+];
 
-self.addEventListener('install', (e) => {
-
-  let cache = caches.open(cacheName).then((c) => {
-    c.addAll([
-      // nothing
-    ]);
-  });
-
-  e.waitUntil(cache);
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(urlsToCache);
+      })
+  );
 });
 
-self.addEventListener('fetch', function (event) {
-
+self.addEventListener('fetch', (event) => {
   event.respondWith(
-
-    caches.open(cacheName).then(function (cache) {
-      return cache.match(event.request).then(function (response) {
-        return response || fetch(event.request).then(function (response) {
-          cache.put(event.request, response.clone());
-          return response;
-        });
-      });
-    })
-
+    caches.match(event.request)
+      .then((response) => {
+        return response || fetch(event.request);
+      })
   );
+});
 
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
 });
